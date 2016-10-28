f2355267ae929a6ae76daf628ea843f9d6eebc5f
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-28 21:53:12 +0200

    Update for new openssl package

--- R/onload.R
+++ R/onload.R
@@ -1,7 +1,7 @@
 .onAttach <- function(lib, pkg){
   keyfile <- agent_dir("id_rsa")
   buf <- readBin(keyfile, raw(), file.info(keyfile)$size)
-  name <- utils::getFromNamespace('parse_pem', 'openssl')(buf)$name
+  name <- names(openssl::read_pem(buf))
   if(!grepl("ENCRYPTED", name, ignore.case = TRUE)){
     packageStartupMessage("Your keystore is currently unprotected! Please set a password using update_password()")
   }
